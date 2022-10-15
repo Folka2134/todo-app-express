4c7c227060f8c9285a931e7a2251a7c9dbe99805
Folka2134 <folka2134@gmail.com>
2022-10-15 11:54:15 +0100

    created basic express server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const PORT = 4000;
+
+app.get("/", (req, res) => {
+  res.send("working");
+});
+
+app.listen(PORT, () => {
+  console.log(`server running on PORT:${PORT}`);
+});
