b46e6ea3f3fa2417989fa8b01e8b4b568e9ec22a
Folka2134 <folka2134@gmail.com>
2022-10-15 12:59:25 +0100

    connected to mongodb with mongoose

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
+// Express setup
 const app = express();
 const PORT = 5000;
+
+// MONGODB Setup
+require("dotenv").config();
+const mongoString = process.env.DATABASE_URL;
+mongoose.connect(mongoString);
+
+const database = mongoose.connection;
+database.on("error", (error) => {
+  console.log(error);
+});
+database.once("connected", () => {
+  console.log("Database Connected");
+});
 
 // Middleware
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
