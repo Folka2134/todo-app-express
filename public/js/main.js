792b1cae36e8e834dc63b499e8ac8aa6be0b7e76
Folka2134 <folka2134@gmail.com>
2022-10-16 12:35:21 +0100

    created main.js and implemented delete functionality

---
+++ public/js/main.js
@@ -0,0 +1,21 @@
+const deleteButton = document.querySelectorAll(".delete");
+
+deleteButton.forEach((element) => {
+  element.addEventListener("click", deleteArtist);
+});
+
+async function deleteArtist() {
+  const todo = this.parentNode.childNodes[1].innerText;
+  try {
+    await fetch("api/deleteTodo", {
+      method: "delete",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        todo: todo,
+      }),
+    });
+    location.reload();
+  } catch (error) {
+    console.log(error);
+  }
+}
