8f4328045e48302f1b637bbeb56c8c8247d6bd32
Folka2134 <folka2134@gmail.com>
2022-10-28 11:04:13 +0100

    cleaned comments

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -3,15 +3,12 @@ const homeRoutes = express.Router();
 const homeController = require("../controllers/home");
 const authController = require("../controllers/auth");
 
-//// GET
-// get todos from database
-// homeRoutes.get("/", homeController.getTodos);
+/*
+// GET
+ */
 
-// Go to homepage
 homeRoutes.get("/", homeController.homePage);
-
 homeRoutes.get("/signup", authController.signup);
-
 homeRoutes.get("/signin", authController.signin);
 
 module.exports = homeRoutes;
