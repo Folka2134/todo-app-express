079c44cd968dc0198764f6f485986c8375976bf6
Folka2134 <folka2134@gmail.com>
2022-10-31 11:53:37 +0000

    added user password hashing

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -13,6 +14,27 @@ const userSchema = new mongoose.Schema({
     require: true,
     type: String,
   },
+});
+
+// hashPassword
+
+userSchema.pre("save", function save(next) {
+  const user = this;
+  if (!user.isModified("password")) {
+    return next();
+  }
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return next(err);
+    }
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+      user.password = hash;
+      next();
+    });
+  });
 });
 
 module.exports = mongoose.model("Users", userSchema);
